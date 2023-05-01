42d59762a75414069cd87332ffd4353eff129e9b
Disy <72013933+disymayufei@users.noreply.github.com>
2023-05-01 12:07:01 +0800

    fix: 修复使用/sit指令时的各种奇葩问题 (#3)
    fix: 各种坐下指令的bug

--- src/main/java/com/github/zly2006/carpetslsaddition/mixin/entity/MixinPlayerEntity.java
+++ src/main/java/com/github/zly2006/carpetslsaddition/mixin/entity/MixinPlayerEntity.java
@@ -1,6 +1,6 @@
 package com.github.zly2006.carpetslsaddition.mixin.entity;
 
-import com.github.zly2006.carpetslsaddition.util.access.PlayerAccess;
+import com.github.zly2006.carpetslsaddition.PlayerAccess;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
@@ -8,7 +8,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.stat.Stat;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,27 +22,11 @@ public abstract class MixinPlayerEntity implements PlayerAccess {
 
     @Shadow public abstract void resetStat(Stat<?> stat);
 
-    @Shadow public abstract Text getName();
-
-    private Text displayName = null;
-
     @Override
     public boolean holdingObsidianPickaxe() {
         ItemStack itemStack = getEquippedStack(EquipmentSlot.MAINHAND);
         NbtCompound compound = itemStack.getSubNbt(MOD_ID);
         return compound != null && compound.contains(ITEM_NAME) && compound.getString(ITEM_NAME).equals(OBSIDIAN_PICKAXE);
-    }
-
-    @Override
-    public void setDisplayName(Text name) {
-        this.displayName = name;
-    }
-
-    @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
-    public void injectGetDisplayName(CallbackInfoReturnable<Text> cir) {
-        if (this.displayName != null) {
-            cir.setReturnValue(displayName);
-        }
     }
 
 
