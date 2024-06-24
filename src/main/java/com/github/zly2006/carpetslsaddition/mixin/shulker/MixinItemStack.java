9d840e59b6cd96d05a7965d0832ca95afc57b527
Liyan Zhao <return65535@qq.com>
2024-06-24 13:50:15 +0800

    fix: emptyShulkerBoxStack

--- src/main/java/com/github/zly2006/carpetslsaddition/mixin/shulker/MixinItemStack.java
+++ src/main/java/com/github/zly2006/carpetslsaddition/mixin/shulker/MixinItemStack.java
@@ -1,23 +1,23 @@
 package com.github.zly2006.carpetslsaddition.mixin.shulker;
 
-import com.github.zly2006.carpetslsaddition.SLSCarpetSettings;
 import com.github.zly2006.carpetslsaddition.util.ShulkerBoxItemUtil;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public class MixinItemStack {
     @Inject(
-            method = "getMaxCount",
+            method = "capCount",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void getMaxCount(CallbackInfoReturnable<Integer> cir) {
-        if (SLSCarpetSettings.emptyShulkerBoxStack && ShulkerBoxItemUtil.isEmptyShulkerBoxItem((ItemStack) (Object) this)) {
-            cir.setReturnValue(ShulkerBoxItemUtil.SHULKERBOX_MAX_STACK_AMOUNT);
+    private void capCount(int maxCount, CallbackInfo ci) {
+        if (ShulkerBoxItemUtil.isEmptyShulkerBoxItem((ItemStack) (Object) this)) {
+            ci.cancel();
+
         }
     }
 }
--- src/main/java/com/github/zly2006/carpetslsaddition/mixin/shulker/MixinScreenHandler.java
+++ src/main/java/com/github/zly2006/carpetslsaddition/mixin/shulker/MixinScreenHandler.java
@@ -1,26 +1,35 @@
 package com.github.zly2006.carpetslsaddition.mixin.shulker;
 
 import com.github.zly2006.carpetslsaddition.util.ShulkerBoxItemUtil;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ScreenHandler.class)
 public abstract class MixinScreenHandler {
-    @WrapOperation(
-            method = "internalOnSlotClick",
+    @Redirect(
+            method = "*",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/item/ItemStack;areItemsAndComponentsEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"
             )
     )
-    private boolean checkEqual(ItemStack stack, ItemStack otherStack, Operation<Boolean> original) {
-        if (ShulkerBoxItemUtil.shulkerBoxEqual(stack, otherStack)) {
-            return true;
-        }
-        return original.call(stack, otherStack);
+    private boolean checkEqual(ItemStack stack, ItemStack otherStack) {
+        if (ShulkerBoxItemUtil.shulkerBoxEqual(stack, otherStack)) return true;
+        return ItemStack.areItemsAndComponentsEqual(stack, otherStack);
+    }
+
+    @Redirect(
+            method = "*",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;isStackable()Z"
+            )
+    )
+    private boolean isStackable(ItemStack stack) {
+        if (ShulkerBoxItemUtil.isEmptyShulkerBoxItem(stack)) return true;
+        return stack.isStackable();
     }
 }
