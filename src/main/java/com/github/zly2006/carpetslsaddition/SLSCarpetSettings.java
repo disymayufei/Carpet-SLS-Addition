948b31867a19d1fc963baf3b1f2b97bc0d640301
Disy <disymayufei@yeah.net>
2024-06-25 00:33:10 +0800

    chore: 标注功能来源

--- src/main/java/com/github/zly2006/carpetslsaddition/SLSCarpetSettings.java
+++ src/main/java/com/github/zly2006/carpetslsaddition/SLSCarpetSettings.java
@@ -3,56 +3,76 @@ package com.github.zly2006.carpetslsaddition;
 import carpet.api.settings.Rule;
 import carpet.api.settings.RuleCategory;
 
-import static com.github.zly2006.carpetslsaddition.ServerMain.CARPET_ID;
-
 public class SLSCarpetSettings {
     public static final String NEED_CLIENT = "needClient";  // 需要客户端安装SLS-Addition或实现相关支持
+    public static final String FROM_AMS = "AMS";
+    public static final String SLSA = "SLS";
 
-    @Rule(categories = {CARPET_ID, RuleCategory.SURVIVAL})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.SURVIVAL})
     public static boolean obtainableReinforcedDeepSlate = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.OPTIMIZATION})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.OPTIMIZATION})
     public static int skipTicksForJoblessVillager = 0;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.CREATIVE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.CREATIVE})
     public static boolean creativeNoInfinitePickup = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.OPTIMIZATION})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.OPTIMIZATION})
     public static boolean noBatSpawning = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean canUseHatCommand = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean canUseSitCommand = false;
 
-    @Rule(categories = {CARPET_ID}, strict = false, options = {"#none"})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA}, strict = false, options = {"#none"})
     public static String botPrefix = "#none";
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean creativeObeyEnchantmentRule = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean fakePlayersNotOccupiedSleepQuota = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.CREATIVE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.CREATIVE})
     public static int maxUpdateQueueSize = -1;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE, NEED_CLIENT})
+    /** From Plusls Carpet Addition **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE, NEED_CLIENT})
     public static boolean emptyShulkerBoxStack = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** From Plusls Carpet Addition **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean playerSit = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean endermanCanPickUpMushroom = true;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.CREATIVE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.CREATIVE})
     public static boolean oldRedstoneConnectionLogic = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.OPTIMIZATION})
+    /** From Carpet AMS Addition **/
+    @Rule(categories = {SLSA, FROM_AMS, RuleCategory.OPTIMIZATION})
     public static boolean optimizedOnDragonRespawn = false;
 
-    @Rule(categories = {CARPET_ID, RuleCategory.FEATURE})
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
+    public static boolean printStackTraceOnVillagerSpawn = false;
+
+    /** Carpet SLS Addition original **/
+    @Rule(categories = {SLSA, RuleCategory.FEATURE})
     public static boolean elytraCraftable = false;
 }
